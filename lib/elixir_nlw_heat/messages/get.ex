0ab3447fd8d7cff71710d24636b03985eb18768f
Marcos Corsi <marcosvcorsi@gmail.com>
2021-11-20 21:29:37 -0300

    feat: add words count task

---
+++ lib/elixir_nlw_heat/messages/get.ex
@@ -0,0 +1,12 @@
+defmodule ElixirNlwHeat.Messages.Get do 
+  import Ecto.Query
+
+  alias ElixirNlwHeat.{Message, Repo}
+
+  def today_messages do
+    today = Date.utc_today()
+    query = from message in Message, where: type(message.inserted_at, :date) == ^today
+
+    Repo.all(query)
+  end
+end
---
+++ lib/elixir_nlw_heat/tags/count.ex
@@ -0,0 +1,19 @@
+defmodule ElixirNlwHeat.Tags.Count do
+  alias ElixirNlwHeat.Messages.Get
+
+  def call do
+    Get.today_messages()
+    |> Task.async_stream(fn message -> count_words(message.text) end)
+    |> Enum.reduce(%{}, fn elem, acc -> sum_values(elem, acc) end);
+  end
+
+  defp count_words(text) do
+    text
+    |> String.split()
+    |> Enum.frequencies()
+  end
+
+  defp sum_values({:ok, map1}, map2) do
+    Map.merge(map1, map2, fn _key, value1, value2 -> value1 + value2 end)
+  end
+end
